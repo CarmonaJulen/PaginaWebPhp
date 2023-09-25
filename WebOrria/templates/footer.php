71c4d6ed5a776c16cf248b746671f2d7a130130c
Julen Carmona <144795313+CarmonaJulen@users.noreply.github.com>
2023-09-25 12:05:21 +0200

    Funciona

--- WebOrria/templates/footer.php
+++ WebOrria/templates/footer.php
@@ -1,3 +1,38 @@
-
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="mt-3">Lista de alumnos</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Edad</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          if ($alumnos && $sentencia->rowCount() > 0) {
+            foreach ($alumnos as $fila) {
+              ?>
+              <tr>
+                <td><?php echo escapar($fila["id"]); ?></td>
+                <td><?php echo escapar($fila["nombre"]); ?></td>
+                <td><?php echo escapar($fila["apellido"]); ?></td>
+                <td><?php echo escapar($fila["email"]); ?></td>
+                <td><?php echo escapar($fila["edad"]); ?></td>
+              </tr>
+              <?php
+            }
+          }
+          ?>
+        <tbody>
+      </table>
+    </div>
+  </div>
+</div>
 </body>
+
 </html>
